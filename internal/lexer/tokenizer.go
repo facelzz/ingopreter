0a07a94123c4cda3c5b1e857c83acc491175811c
facelzz <illia.facelzz@gmail.com>
2026-02-20 23:39:11 +0200

    feat: process by first token char with switch, use greedy capture
    dropped ifs in favor of default switch implementation, capture single-char tokens
    
    known issues:
    - skips multi-char operators and punctuation
    - skips numeric literals
    - skips unexpected tokens
    - code block testcase fails

--- internal/lexer/tokenizer.go
+++ internal/lexer/tokenizer.go
@@ -2,6 +2,7 @@ package lexer
 
 import (
 	"log/slog"
+	"slices"
 	"unicode"
 )
 
@@ -9,51 +10,57 @@ type Token struct {
 	Value string
 }
 
+var singleCharTokens = []rune{
+	'(', ')', '[', ']', '{', '}', ',', ';', '!', '~', '"',
+}
+
 // Tokenize TODO: line-by-line might no work because of multi-line tokens
 func Tokenize(chars []rune) []Token {
 	var tokens []Token // TODO: here's some random capacity
 
-	capturingToken := false
-	tokenStart := 0
-	for i, v := range chars {
+	var v rune
+	for i := 0; i < len(chars); i++ {
+		v = chars[i]
 		slog.Debug("processing rune",
 			"runeIndex", i,
 			"runeValue", string(v))
 
-		// capture token start
-		if !unicode.IsSpace(v) && !capturingToken {
-			slog.Debug("Word start!")
+		switch {
+		// single char token
+		case slices.Contains(singleCharTokens, v):
+			tokens = append(tokens, Token{
+				Value: string(chars[i : i+1]),
+			})
+			slog.Debug("single-char token captured")
 
-			capturingToken = true
-			continue
-		}
+		// skip
+		case unicode.IsSpace(v):
+			slog.Debug("space skipped")
 
-		// capture terminated token
-		if unicode.IsSpace(v) {
-			if capturingToken {
-				slog.Debug("Word end!")
-
-				// Capture token
-				tokens = append(tokens, Token{
-					Value: string(chars[tokenStart:i]),
-				})
+		// starts with letter - identifier ar keywork
+		// EBNF: identifier = letter { letter | unicode_digit } .
+		case chsp(v, letter):
+			// greedily capture token
+			j := i + 1
+			for {
+				if j >= len(chars) || !chsp(chars[j], letter) && !chsp(chars[j], unicodeDigit) {
+					break
+				}
+				j++
 			}
 
-			tokenStart = i + 1
-			capturingToken = false
-			continue
+			// capture token
+			tokens = append(tokens, Token{
+				Value: string(chars[i:j]),
+			})
+			i = j - 1
+			slog.Debug("identifier or keyword captured")
+
+		// unexpected
+		default:
+			// Unknown
+			//panic("Unknown char: " + string(v))
 		}
-	}
-	// capture non-closed token
-	if capturingToken {
-		slog.Debug("Word end!")
-
-		// Capture token
-		tokens = append(tokens, Token{
-			Value: string(chars[tokenStart:]),
-		})
-
-		capturingToken = false
 	}
 
 	if len(tokens) == 0 {
